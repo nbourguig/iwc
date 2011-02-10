4576cf5aab92c8167cd40bfd6cd40419d83ce609
nbourguig <nbourguig@gmail.com>
2011-02-10 21:54:26 +0100

    DONE / Coming soon

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-/* Author: 
-
-*/
+$(document).ready(function(){
 
 
+    // try center everything like it should ...
+    $(window).resize(function(){
+        $('.comingsoon').css({
+            position:'absolute',
+            left: ($(window).width()
+                - $('.comingsoon').outerWidth())/2,
+            top: ($(window).height()
+                - $('.comingsoon').outerHeight())/2
+        });
 
+    });
+    $(window).resize();
 
+    
 
+});
 
 
 
